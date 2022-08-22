d0aaf76a337d9ab10dba0ddacfe4ec321e080f1a
Ziba Parsons <zb.ghsm@gmail.com>
2022-08-22 11:28:15 -0400

    pFedMe user is modified to have beta shrinkage

--- FLAlgorithms/users/userpFedMe.py
+++ FLAlgorithms/users/userpFedMe.py
@@ -60,9 +60,13 @@ class UserpFedMe(User):
 
                     param_x.data = param_y.data - self.learning_rate * (param_x.grad.data - param_z.data) #* param_x.grad.data #
                     param_z.data = param_z.data + self.beta / self.learning_rate * (param_x.data - param_y.data)
-                    param_y.data = param_y.data + 1 / num_users * (
-                            param_x.data - param_z.data * self.learning_rate / self.beta - param_x_old.data + param_z_old.data * self.learning_rate / self.beta)
+                    # param_y.data = param_y.data + 1 / num_users * (
+                    #         param_x.data - param_z.data * self.learning_rate / self.beta - param_x_old.data + param_z_old.data * self.learning_rate / self.beta)
 
+                    param_y.data = param_y.data + 1 / num_users * (
+                            param_x.data - param_z.data * self.learning_rate - param_x_old.data + param_z_old.data * self.learning_rate)
+
+            self.beta = 0.95 * self.beta
             persionalized_model_bar = copy.copy(self.local_model_x)
 
             # update local weight after finding aproximate theta
